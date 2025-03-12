81652f4b62b76b4eaf59d1025b145649f4ae1ad9
ohdaeo <assa01389@gmail.com>
2025-03-12 09:51:53 +0900

    layout 컴포넌트 구분하기

--- src/app/(with-search)/search/page.tsx
+++ src/app/(with-search)/search/page.tsx
@@ -3,7 +3,7 @@ import GoodItem from "@/components/good-item";
 import goods from "@/mock/good.json";
 // 쿼리 처리하기
 // 아래 페이지는 쿼리를 서버에서 읽어들여서 처리함.
-// http://localhost:3000/search?keword=iu
+// 주소/search?keword=iu
 export default async function Page({
   searchParams,
 }: {
